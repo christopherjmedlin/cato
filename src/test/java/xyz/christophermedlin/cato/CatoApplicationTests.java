41cdfda9e50438ab70dc86904130032f7a7933da
Christopher Medlin <cmedlin@unm.edu>
2023-01-18 17:18:00 -0700

    Initial commit

---
+++ src/test/java/xyz/christophermedlin/cato/CatoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.christophermedlin.cato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
