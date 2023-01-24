2de7a6a2b4d46a2365e99dd7d165493b1b604636
Christopher Medlin <christopherjmedlin@mailbox.org>
2023-01-24 14:06:54 -0700

    add getSmoothiesByIngredientIds to service

--- frontend/src/app/smoothie-list/smoothie-list.component.ts
+++ frontend/src/app/smoothie-list/smoothie-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Smoothie } from '../smoothie';
+import { SmoothieService } from '../smoothie.service';
 
 @Component({
   selector: 'app-smoothie-list',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./smoothie-list.component.css']
 })
 export class SmoothieListComponent {
+  constructor(private route: ActivatedRoute,
+              private smoothieService : SmoothieService) {}
 
+  ngOnInit() {
+    this.route.queryParams
+      .subscribe(params => {
+        this.smoothieService.getSmoothiesByIngredientIds(
+          params["ingredientIds"]
+        )
+        .subscribe(s => {
+          this.smoothies = s;
+        })
+      });
+  }
+
+  smoothies: Smoothie[] = []
 }
